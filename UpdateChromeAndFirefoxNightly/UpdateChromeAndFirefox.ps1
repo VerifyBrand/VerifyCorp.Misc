d0e8984467cb2a1d2ba2bd188b96cb6ef67da1ab
Jay Hilden <Jay.Hilden@verifycorporation.com>
2015-01-29 13:11:32 -0600

    script to update FF and chrome every night

---
+++ UpdateChromeAndFirefoxNightly/UpdateChromeAndFirefox.ps1
@@ -0,0 +1,5 @@
+﻿choco update chrome
+
+choco update firefox
+
+#Read-Host -Prompt "Press Enter to exit"
---
+++ UpdateChromeAndFirefoxNightly/UpdateChromeAndFirefoxLauncher.ps1
@@ -0,0 +1,13 @@
+﻿# Launch the process and wait for it to finish
+try
+{
+    $p = Start-Process "$PsHome\PowerShell.exe" -Verb RunAs -ArgumentList "-file $PSScriptRoot\UpdateChromeAndFirefox.ps1" -PassThru 
+    $p.WaitForExit();
+    Write-Host "exit code: " + $p.ExitCode
+}
+catch
+{
+    $Error[0] # Dump details about the last error
+    exit 1
+}
+
